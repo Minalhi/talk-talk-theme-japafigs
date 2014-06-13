0b723a6446018a3c417ace7110f7202363a12033
Nicolas Goussin <innuko@gmail.com>
2014-06-13 23:27:08 +0000

    MAJ WIP Sub-header

--- includes/common/sub-header.php
+++ includes/common/sub-header.php
@@ -4,12 +4,13 @@
         <img src="img/common/sub-header/top-picture.png">
       </div>
       <ul class="forums-list clearfix">
+        <?php // for ($i=0; $i < 3; $i++): ?> 
         <li class="forum">
           <div class="forum-picture">
             <img src="img/common/sub-header/japazone.png" alt="">
           </div>
           <div class="forum-desc"
-               style="border-left: solid red 4px;">
+               style="border-left: solid #d15e5d 4px;">
             <h4 class="forum-title"><a href="#">Japa Zone</a></h4>
             <ul class="forum-children">
               <li><a href="#">Présentez-vous !</a></li>
@@ -17,10 +18,13 @@
             </ul>
           </div>
         </li>
+        
         <li class="forum">
-          <div class="forum-picture"></div>
+          <div class="forum-picture">
+             <img src="img/common/sub-header/figurines.png" alt="">
+          </div>
           <div class="forum-desc"
-               style="border-left: solid blue 4px;">
+               style="border-left: solid #57a19e 4px;">
             <h4 class="forum-title"><a href="#">Figurines PVC &amp; résines</a></h4>
             <ul class="forum-children">
               <li><a href="#">Nouveautés figurines</a></li>
@@ -28,6 +32,71 @@
             </ul>
           </div>
         </li>
-      </ul>
+        
+         <li class="forum">
+          <div class="forum-picture">
+             <img src="img/common/sub-header/toys.png" alt="">
+          </div> 
+          <div class="forum-desc"
+               style="border-left: solid #e9ac41 4px;">
+            <h4 class="forum-title"><a href="#">Toys PVC / Metal &amp; Dolls</a></h4>
+            <ul class="forum-children">
+              <li><a href="#">Nouveautés Toys &amp; Dolls</a></li>
+              <li><a href="#">Discussions Toys &amp; Dolls</a></li>
+            </ul>
+          </div>
+        </li>     
+        
+         <li class="forum">
+          <div class="forum-picture">
+             <img src="img/common/sub-header/boutiques-achats.png" alt="">
+          </div> 
+          <div class="forum-desc"
+               style="border-left: solid #3c855c 4px;">
+            <h4 class="forum-title"><a href="#">Boutiques &amp; Achats</a></h4>
+            <ul class="forum-children">
+              <li><a href="#">The Japa Shop</a></li>
+              <li><a href="#">The Japa Search</a></li>
+              <li><a href="#">Les boutiques en ligne</a></li>
+              <li><a href="#">Les boutiques physiques</a></li>
+            </ul>
+          </div>
+        </li>
+        
+        <li class="forum">
+          <div class="forum-picture">
+             <img src="img/common/sub-header/divers.png" alt="">
+          </div>
+          <div class="forum-desc"
+               style="border-left: solid #a73e5c 4px;">
+            <h4 class="forum-title"><a href="#">Divers</a></h4>
+            <ul class="forum-children">
+              <li><a href="#">Garage Kits</a></li>
+              <li><a href="#">Hors-sujet</a></li>
+              <li><a href="#">JapaFigs</a></li>
+              <li><a href="#">Événements &amp; Rencontres</a></li>
+            </ul>
+          </div>
+        </li>
+        
+                
+         <li class="forum">
+          <div class="forum-picture">
+             <img src="img/common/sub-header/culture.png" alt="">
+          </div>  
+          <div class="forum-desc"
+               style="border-left: solid #78a3b9 4px;">
+            <h4 class="forum-title"><a href="#">Cultures</a></h4>
+            <ul class="forum-children">
+              <li><a href="#">Animations, Mangas</a></li>
+              <li><a href="#">Jeux-vidéo, Geekeries</a></li>
+              <li><a href="#">Dramas, Ciné, Séries TV</a></li>
+            </ul>
+          </div>
+        </li>     
+        
+        
+        <?php //endfor ?>
+      </ul><!-- end .forums-list -->
   
     </div><!--end .sub-header -->
